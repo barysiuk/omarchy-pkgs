335d4f7490751839270adad2c6185426b8929876
Sergey Barysiuk <s.barysiuk@gmail.com>
2026-09-07 12:55:26 -0500

    Configure Surface DTB through ukify

--- pkgbuilds/omarchy-surface-pro-12/test.sh
+++ pkgbuilds/omarchy-surface-pro-12/test.sh
@@ -7,15 +7,17 @@ python3 - "$dtb" <<'PY'
 import struct, sys
 p=open(sys.argv[1], 'rb').read(); assert p[:4] == b'\xd0\r\xfe\xed'; assert b'microsoft,surface-pro-12in\0' in p[:struct.unpack_from('>I',p,4)[0]]
 PY
-grep -Fqx 'MKINITCPIO_UKI_OPTIONS="--dtb /usr/lib/omarchy-surface-pro-12/x1p42100-microsoft-sp12in.dtb"' "$dir/omarchy-surface-pro-12.conf"
+grep -Fqx 'MKINITCPIO_UKI_OPTIONS="--ukiconfig /etc/kernel/uki-surface-pro-12.conf"' "$dir/omarchy-surface-pro-12.conf"
+grep -Fqx 'DeviceTree=/usr/lib/omarchy-surface-pro-12/x1p42100-microsoft-sp12in.dtb' "$dir/uki-surface-pro-12.conf"
+! grep -Fq -- '--dtb' "$dir/omarchy-surface-pro-12.conf"
 grep -Fqx 'ENABLE_UKI=yes' "$dir/omarchy-surface-pro-12.conf"
 grep -Fqx 'CUSTOM_UKI_NAME=omarchy' "$dir/omarchy-surface-pro-12.conf"
 grep -Fqx 'MKINITCPIO_FALLBACK=yes' "$dir/omarchy-surface-pro-12.conf"
 grep -Fqx 'KERNEL_CMDLINE[default]+="initramfs_async=0 clk_ignore_unused pd_ignore_unused arm64.nopauth"' "$dir/omarchy-surface-pro-12.conf"
 ! grep -Eq 'archiso|systemd\.tpm2_wait|modprobe\.blacklist|debug' "$dir/omarchy-surface-pro-12.conf"
 grep -Fqx "arch=('aarch64')" "$dir/PKGBUILD"
-for dep in limine-mkinitcpio-hook limine-snapper-sync mkinitcpio snapper; do
-  grep -Fqx "depends=('limine-mkinitcpio-hook' 'limine-snapper-sync' 'mkinitcpio' 'snapper')" "$dir/PKGBUILD"
+for dep in limine-mkinitcpio-hook limine-snapper-sync mkinitcpio snapper systemd-ukify; do
+  grep -Fqx "depends=('limine-mkinitcpio-hook' 'limine-snapper-sync' 'mkinitcpio' 'snapper' 'systemd-ukify')" "$dir/PKGBUILD"
 done
 [[ $(jq -r '.source' "$dir/.omarchy/package.json") == local ]]
 [[ $(jq -r '.channels[]' "$dir/.omarchy/package.json") == edge ]]
